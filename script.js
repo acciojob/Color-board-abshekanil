494f5a78d465315ae49c6c0634684a8f2300b583
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-06 19:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded' , function(){
+	const squares = document.querySelectorAll('.square');
+
+	squares.forEach(square => {
+		square.addEventListener('mouseover', function(){
+			square.style.backgroundColor = '#e74c3c';
+		});
+
+		 square.addEventListener('mouseout', function() {
+      setTimeout(() => {
+        square.style.backgroundColor = '#3498db';
+      }, 1000);
+			 
+		 });
+			 
+	});
+});
